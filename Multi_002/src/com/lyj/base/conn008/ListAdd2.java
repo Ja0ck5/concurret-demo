4c44834818b24a5d1009002a1948578661134635
ja0ck5 <975117619@qq.com>
2017-02-09 00:38:03 +0800

    使用CountDownLatch 优化线程通信

--- Multi_002/src/com/lyj/base/conn008/ListAdd2.java
+++ Multi_002/src/com/lyj/base/conn008/ListAdd2.java
@@ -25,7 +25,7 @@ public class ListAdd2 {
 		
 		// 1 实例化出来一个 lock
 		// 当使用wait 和 notify 的时候 ， 一定要配合着synchronized关键字去使用
-		final Object lock = new Object();
+//		final Object lock = new Object();
 		
 		final CountDownLatch countDownLatch = new CountDownLatch(1);
 		
@@ -33,7 +33,7 @@ public class ListAdd2 {
 			@Override
 			public void run() {
 				try {
-					synchronized (lock) {
+//					synchronized (lock) {
 						for(int i = 0; i <10; i++){
 							list2.add();
 							System.out.println("当前线程：" + Thread.currentThread().getName() + "添加了一个元素..");
@@ -41,10 +41,10 @@ public class ListAdd2 {
 							if(list2.size() == 5){
 								System.out.println("已经发出通知..");
 								countDownLatch.countDown();
-								lock.notify();//通知，是不释放锁的
+//								lock.notify();//通知，是不释放锁的
 							}
 						}						
-					}//临界区的代码执行完才释放锁
+					//}//临界区的代码执行完才释放锁,对此延时使用 CountDownLatch
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
@@ -55,11 +55,11 @@ public class ListAdd2 {
 		Thread t2 = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				synchronized (lock) {
+//				synchronized (lock) {
 					if(list2.size() != 5){
 						try {
 							System.out.println("t2进入...");
-							lock.wait();//等待，释放锁
+//							lock.wait();//等待，释放锁
 							countDownLatch.await();
 						} catch (InterruptedException e) {
 							e.printStackTrace();
@@ -67,7 +67,7 @@ public class ListAdd2 {
 					}
 					System.out.println("当前线程：" + Thread.currentThread().getName() + "收到通知线程停止..");
 					throw new RuntimeException();
-				}
+//				}
 			}
 		}, "t2");	
 		
