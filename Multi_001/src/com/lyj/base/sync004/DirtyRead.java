e9c23f7f114cfed91d66bb033852983cca72a2b8
ja0ck5 <975117619@qq.com>
2017-02-08 22:03:25 +0800

    add comment

--- Multi_001/src/com/lyj/base/sync004/DirtyRead.java
+++ Multi_001/src/com/lyj/base/sync004/DirtyRead.java
@@ -1,12 +1,10 @@
 package com.lyj.base.sync004;
 /**
  * 业务整体需要使用完整的synchronized，保持业务的原子性。
- * @author alienware
- *
  */
 public class DirtyRead {
 
-	private String username = "bjsxt";
+	private String username = "haha";
 	private String password = "123";
 	
 	public synchronized void setValue(String username, String password){
@@ -29,12 +27,13 @@ public class DirtyRead {
 	
 	
 	public static void main(String[] args) throws Exception{
-		
+/*		getValue方法得到：username = hehe , password = 123
+				setValue最终结果：username = hehe , password = 456*/
 		final DirtyRead dr = new DirtyRead();
 		Thread t1 = new Thread(new Runnable() {
 			@Override
 			public void run() {
-				dr.setValue("z3", "456");		
+				dr.setValue("hehe", "456");		
 			}
 		});
 		t1.start();
