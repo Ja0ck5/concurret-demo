67de48fea850291769a491e8ce219a9e668e1a04
ja0ck5 <975117619@qq.com>
2017-02-08 23:15:57 +0800

    add volatile comment

--- Multi_001/src/com/lyj/base/sync007/RunThread.java
+++ Multi_001/src/com/lyj/base/sync007/RunThread.java
@@ -2,6 +2,9 @@ package com.lyj.base.sync007;
 
 public class RunThread extends Thread{
 
+	/**
+	 * volatile 保证可见性
+	 */
 	private volatile boolean isRunning = true;
 	private void setRunning(boolean isRunning){
 		this.isRunning = isRunning;
@@ -10,6 +13,9 @@ public class RunThread extends Thread{
 	public void run(){
 		System.out.println("进入run方法..");
 		int i = 0;
+		/*加了 volatile 修饰，修改了变量之后会写会主存，从而可见 此时 isRunning 为 false. 
+		 * 如果没有 volatile 修饰，那么是不可见的，这里仍然为 true 一直空轮询
+		 * */
 		while(isRunning == true){
 			//..
 		}
