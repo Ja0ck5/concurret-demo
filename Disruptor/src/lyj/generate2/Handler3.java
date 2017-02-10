f4a1dc127765639e6d3c49d1e48c60f36910d572
ja0ck5 <975117619@qq.com>
2017-02-10 22:00:10 +0800

    change

---
+++ Disruptor/src/lyj/generate2/Handler3.java
@@ -0,0 +1,12 @@
+package lyj.generate2;
+
+import com.lmax.disruptor.EventHandler;
+
+import lyj.generate1.Trade;
+
+public class Handler3 implements EventHandler<Trade> {
+    @Override  
+    public void onEvent(Trade event, long sequence,  boolean endOfBatch) throws Exception {  
+    	System.out.println("handler3: name: " + event.getName() + " , price: " + event.getPrice() + ";  instance: " + event.toString());
+    }  
+}
